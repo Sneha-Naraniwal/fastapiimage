95aafd85c2fc55e28abace90d28ecca9088f98c3
Sneha-Naraniwal <150178875+Sneha-Naraniwal@users.noreply.github.com>
2025-04-24 15:55:52 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hello, FastAPI with Docker and GitHub Actions! from Sneha's side checking AGAIN "}
+    return {"message": "Hello, FastAPI with Docker and GitHub Actions! from Sneha's side checking AGAIN DOCKER  "}
